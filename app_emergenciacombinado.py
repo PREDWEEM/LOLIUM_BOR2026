07ae9fcdffab7865668ac37bc92d31bc4716ed92
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-03-12 10:51:38 -0300

    Update app_emergenciacombinado.py

--- app_emergenciacombinado.py
+++ app_emergenciacombinado.py
@@ -194,7 +194,7 @@ df = get_data(archivo_usuario)
 
 st.sidebar.divider()
 st.sidebar.markdown("**Parámetros de Emergencia**")
-umbral_er = st.sidebar.slider("Umbral Tasa Diaria (Para detectar pico)", 0.05, 0.80, 0.05)
+umbral_er = st.sidebar.slider("Umbral Tasa Diaria (Para detectar pico)", 0.025, 0.80, 0.025)
 
 st.sidebar.divider()
 st.sidebar.markdown("🌡️ **Fisiología Térmica (Bio-Limit)**")
