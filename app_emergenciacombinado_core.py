29052233020decf1b2694268d8abc13f6de91655
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-07-28 15:40:57 -0300

    Update app_emergenciacombinado_core.py

--- app_emergenciacombinado_core.py
+++ app_emergenciacombinado_core.py
@@ -683,7 +683,7 @@ calentamiento_suelo = st.sidebar.slider(
 
 st.sidebar.divider()
 st.sidebar.markdown("## 💧 4. Balance Hídrico (Suelo)")
-w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=20.0, step=1.0)
+w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=18.81, step=1.0)
 exponente_kr = st.sidebar.slider(
     "Exponente Kr (secado superficial)",
     min_value=0.0,
